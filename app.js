afb3eb7d617a425efb2125ff3cbd660f155d5995
elodiebhs <elodiebouthors@hotmail.com>
2021-08-04 01:16:39 +0000

    Add an Array of Images

--- app.js
+++ app.js
@@ -9,12 +9,30 @@ const background = (<img
   alt="ocean"
   src="/images/ocean.jpg" />)
 
+const images =[];
+
+for (const animal in animals) {
+  images.push( 
+    <img
+      key={animal}
+      className='animal'
+      alt={animal}
+      src={animals[animal].image}
+      ariaLabel={animal}
+      role='button'
+    />
+  )
+}
+
 const animalFacts = (
   <div>
     <h1>
       {title === "" ? 'Click an animal for a fun fact!' : title}
     </h1>
     {background}
+    <div classeName='animals'>
+      {images}
+    </div>
   </div>
 );
 
