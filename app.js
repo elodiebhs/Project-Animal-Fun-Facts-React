533a6a59fe0e0da177e3bf122a5fdc1f0fdd0bc8
elodiebhs <elodiebouthors@hotmail.com>
2021-08-04 00:48:05 +0000

    Add a Background

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import { animals } from './animals';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 const title = '';
-const animalFacts = (
-  <h1>
-  {title===""? 'Click an animal for a fun fact!' : title}
-  </h1>);
 
-ReactDOM.render(animalFacts,document.getElementById('root'));
+const background = (<img
+  className="background"
+  alt="ocean"
+  src="/images/ocean.jpg" />)
+
+const animalFacts = (
+  <div>
+    <h1>
+      {title === "" ? 'Click an animal for a fun fact!' : title}
+    </h1>
+    {background}
+  </div>
+);
+
+ReactDOM.render(animalFacts, document.getElementById('root'))
